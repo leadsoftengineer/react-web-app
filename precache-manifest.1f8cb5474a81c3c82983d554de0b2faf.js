862e19450738a87b20513423d00510153560b0e4
smolinks <smolinks.com@hotmail.com>
2019-07-10 19:40:07 +0300

    Updates

---
+++ precache-manifest.1f8cb5474a81c3c82983d554de0b2faf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78aef61106b3496d75e232842a42305",
+    "url": "/react-web-app/index.html"
+  },
+  {
+    "revision": "6271ed4255fe3f2915ff",
+    "url": "/react-web-app/static/css/main.fe6ab0d6.chunk.css"
+  },
+  {
+    "revision": "89265b19dc6a9a157f17",
+    "url": "/react-web-app/static/js/2.a9af3d4b.chunk.js"
+  },
+  {
+    "revision": "6271ed4255fe3f2915ff",
+    "url": "/react-web-app/static/js/main.57d735ff.chunk.js"
+  },
+  {
+    "revision": "627381d5a5a5498707fe",
+    "url": "/react-web-app/static/js/runtime~main.a756a28e.js"
+  }
+]);
